5dfe6b0bc570514ad2e9a7d9422a4a471adde6cd
macroPython <161464674+macroPython@users.noreply.github.com>
2024-02-27 14:28:55 +0200

    Update camhttp.py

--- camhttp.py
+++ camhttp.py
@@ -8,7 +8,7 @@ print(response.text)
 t = json.loads(response.text)
 token = t[0]["value"]["Token"]["name"]
 
-#token = "dd5f1e16f0501b4"
+#token = "dd5f3e16f0101b4"
 url = "https://192.168.1.104/api.cgi?cmd=SetIsp&token=" + token
 #CmdJson = [{"cmd": "SetIsp", "action": 0, "param": {"Isp": {"exposure": "Anti-Smearing","shutter" : {"max" : 125,"min" : 100}}}}]
 CmdJson = [{"cmd": "SetIsp", "action": 0, "param": {"Isp": {"exposure": "Auto","shutter" : {"max" : 125,"min" : 100}}}}]
